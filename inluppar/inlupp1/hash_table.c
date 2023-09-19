de33a9193ad2fb0ad9bf11201190e72aa06ec5fa
AntonEckervald <anton.eckervald@gmail.com>
2023-09-19 18:31:08 +0200

    c

---
+++ inluppar/inlupp1/hash_table.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
+#include <string.h>
+#include <ctype.h>
+#include <time.h>
+#include "hash_table.h"
+
+ioopm_hash_table_t *ioopm_hash_table_create(void) {
+ // TODO: Stub
+  return NULL;
+}
+
+void ioopm_hash_table_destroy(ioopm_hash_table_t *ht) {
+  // TODO: Stub
+ return;
+}
